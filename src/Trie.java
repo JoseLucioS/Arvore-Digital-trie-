0aa201a914a76e89c455f16b936f011f905d96b8
Lucio_Silva <dragaolucio@gmail.com>
2019-11-27 21:34:32 -0300

    inicio do metodo de autocompletar

--- src/Trie.java
+++ src/Trie.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class Trie {
     private TrieNode root;
@@ -65,11 +68,31 @@ public class Trie {
     //TODO fazer o método de remoção
     public void remove(String text){
         if(this.search(text)){
-
-        } else {
             
+        } else {
+
         }
     }
 
     //TODO fazer método de autocompletar
+    public List<String> autocomplete(String prefix){
+        HashMap<Character, TrieNode> child = root.getChildren();
+        List<String> words = new ArrayList<String>();
+        String word;
+        TrieNode node;
+
+        //percorrer até achar node com o último caracter do prefixo, só então começar a procurar por palavras
+        //correspondentes e guardá-las no arraylist
+//        for(){
+//
+//        }
+
+        Collections.sort(words);
+        return words;
+    }
+
+    //TODO fazer a sobrecarga do método de autocompletar
+//    public List<String> autocomplete(String prefix, int size){
+//
+//    }
 }
